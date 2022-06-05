199d24ca8b9eb05b8dbe894b7d5410f5b9fe8ace
aqsa286 <aqsafarooq402@gmail.com>
2022-06-05 21:22:19 +0500

    My Second comment

--- branch.sh
+++ branch.sh
@@ -1,14 +1,11 @@
 FROMBRANCH=$1
 TOBRANCH=$2
 
-# configure your fetch by adding this to your configuration before the fetch command
-git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-# You can check what exists via
 git show-ref
 git remote update
 git fetch --all
 git checkout $TOBRANCH
-ls -la
+ls -la	
 git merge --no-ff --no-edit $FROMBRANCH
 LASTSTATUS=$?
 echo "last status of git merge was $LASTSTATUS"
@@ -17,5 +14,6 @@ then
     echo "The merge failed."
     exit 1  
 fi
-git push
-git push --tags
+git push -u origin $TOBRANCH
+git push -u origin $FROMBRANCH
+
